9951526b71e6837403aaa747687aa2c65c4996a6
unknown <cbelden@cbelden.adage.adagetechnologies.com>
2014-12-01 23:29:16 +0000

    Updated options page to update the snowflakify canvas when saved. Updated snowflakify to handle requestAnimationFrame calls to make the client (content.js) simpler

--- content/content.js
+++ content/content.js
@@ -1,34 +1,35 @@
-var requestID = null;
+(function(chrome, Snowflakify) {
+    var requestID = null;
 
-chrome.storage.local.get('snowflakify-options', function(data) {
-    initializeContent(data['snowflakify-options']);
-});
+    chrome.storage.local.get('snowflakify-options', function(data) {
+        initializeContent(data['snowflakify-options']);
+    });
 
-function initializeContent(options){
-    // TODO move image url to the options page: allow users to specify the snowflake image
-    options = options || {};
-    options.imageUrl = options.imageUrl || chrome.extension.getURL('images/snowflake.png');
+    function initializeContent(options){
+        // TODO move image url to the options page: allow users to specify the snowflake image
+        options = options || {};
+        options.imageUrl = options.imageUrl || chrome.extension.getURL('images/snowflake.png');
 
-    // Start the snow storm!
-    Snowflakify.initialize(options);
-    requestID = requestAnimationFrame(animate);
+        // Start the snow storm!
+        Snowflakify.initialize(options);
+        Snowflakify.startAnimation();
 
-    // Listen to the navbar action
-    chrome.runtime.onMessage.addListener(toggleAnimation);
-}
-
-function animate(t) {
-    Snowflakify.animate(t);
-    requestID = requestAnimationFrame(animate);
-}
-
-function toggleAnimation(request, sender) {
-    if (requestID) {
-        cancelAnimationFrame(requestID);
-        Snowflakify.cancelAnimation();
-        requestID = null;
+        // Listen to the navbar action
+        chrome.runtime.onMessage.addListener(toggleAnimation);
     }
-    else {
+
+    function animate(t) {
+        Snowflakify.animate(t);
         requestID = requestAnimationFrame(animate);
     }
-}
+
+    function toggleAnimation(request, sender) {
+        Snowflakify.toggleAnimation();
+    }
+
+    function stopAnimation() {
+        cancelAnimationFrame(Snowflakify.requestID);
+        Snowflakify.cancelAnimation();
+        Snowflakify.requestID = null;
+    }
+})(chrome, Snowflakify);
--- options/options.js
+++ options/options.js
@@ -1,60 +1,81 @@
-var density  = document.getElementById('density'),
-    speed    = document.getElementById('speed'),
-    rotation = document.getElementById('rotation'),
-    opacity  = document.getElementById('opacity'),
-    image    = document.getElementById('imageUrl'),
-    save     = document.getElementById('save');
+(function(Snowflakify, chrome) {
+    var density  = document.getElementById('density'),
+        speed    = document.getElementById('speed'),
+        rotation = document.getElementById('rotation'),
+        opacity  = document.getElementById('opacity'),
+        image    = document.getElementById('imageUrl'),
+        save     = document.getElementById('save'),
+        controls = [density, speed, rotation, opacity, image],
 
-var controls = [density, speed, rotation, opacity, image];
+        defaults = {
+            density: 50,
+            speed: 50,
+            rotation: 50,
+            opacity: 100,
+            imageUrl: '',
+        };
 
-// Get stored options data and initialize the sliders once retrieved
-chrome.storage.local.get('snowflakify-options', function(data){
-    initializeControls(data['snowflakify-options'], controls);
-});
+    // Get stored options data and initialize the sliders once retrieved
+    chrome.storage.local.get('snowflakify-options', function(data){
+        initializeControls(controls, defaults, data['snowflakify-options']);
+    });
 
-// Subscribe to the Save event
-save.onclick = function(e) {
-    e.preventDefault();
-    var options = {},
-        i = 0,
-        control = null;
+    // Start snowflakify animation
+    Snowflakify.initialize({imageUrl: chrome.extension.getURL('images/snowflake.png')});
+    Snowflakify.startAnimation();
 
-    // Construct options object
-    for(i; i < controls.length; i++) {
-        control = controls[i];
-        if (control.type == 'range') options[control.id] = parseInt(control.value, 10);
-        else options[control.id] = control.value;
+    // Subscribe to the Save event
+    save.onclick = function(e) {
+        e.preventDefault();
+        var options = {},
+            i = 0,
+            control = null;
+
+        // Construct options object
+        for(i; i < controls.length; i++) {
+            control = controls[i];
+            if (control.type == 'range') options[control.id] = parseInt(control.value, 10);
+            else options[control.id] = control.value;
+        }
+
+        chrome.storage.local.set({'snowflakify-options': options});
+
+        save.className = "button";
+        save.value = "Saved!";
+
+        // Restart the animation so user can see updated settings in action!
+        options.imageUrl || (options.imageUrl = chrome.extension.getURL('images/snowflake.png'));
+        Snowflakify.restartAnimation(options);
+    };
+
+    function initializeControls(controls, defaults, options) {
+        var val = null,
+            i = 0,
+            control = null;
+
+        options = options || {};
+        defaults = defaults || {};
+
+        // Set the slider value of each control
+        for(i; i < controls.length; i++) {
+            control = controls[i];
+
+            // Get saved/default value for this control
+            val = options[control.id] || defaults[control.id];
+
+            // Ensure value is valid for range controls
+            if (control.type === 'range' && (!val || typeof(val) !== 'number')) val = 50;
+            if (val < 0) val = 0;
+            if (val > 100) val = 100;
+
+            control.value = val;
+            control.onmousedown = saveDirty;
+        }
     }
 
-    chrome.storage.local.set({'snowflakify-options': options});
-
-    save.className = "button";
-    save.value = "Saved!";
-};
-
-function initializeControls(options, controls) {
-    var val = null,
-        i = 0,
-        control = null;
-
-    // Set the slider value of each control
-    for(i; i < controls.length; i++) {
-
-        control = controls[i];
-        val = options[control.id];
-
-        // Set default value for range controls if input is missing or invalid
-        if (control.type == 'range' && !val) val = 50;
-        if (val < 0) val = 0;
-        if (val > 100) val = 100;
-
-        control.value = val;
-
-        control.onmousedown = saveDirty;
+    function saveDirty(e) {
+        save.className = "dirty button";
+        save.value = "Save";
     }
-}
 
-function saveDirty(e) {
-    save.className = "dirty button";
-    save.value = "Save";
-}
+})(Snowflakify, chrome);
