c20c0f31a6c0823600a9011b9ddf79816306d5ee
unknown <cbelden@cbelden.adage.adagetechnologies.com>
2014-12-08 19:16:26 +0000

    Added immediate animation reset when settings are updated on the options page. User gets immediate feedback.

--- options/options.js
+++ options/options.js
@@ -1,11 +1,12 @@
-(function(Snowflakify, chrome) {
-    var density  = document.getElementById('density'),
-        speed    = document.getElementById('speed'),
-        rotation = document.getElementById('rotation'),
-        opacity  = document.getElementById('opacity'),
-        image    = document.getElementById('imageUrl'),
-        save     = document.getElementById('save'),
-        controls = [density, speed, rotation, opacity, image],
+(function(Snowflakify, _, chrome) {
+    var density       = document.getElementById('density'),
+        speed         = document.getElementById('speed'),
+        rotation      = document.getElementById('rotation'),
+        opacity       = document.getElementById('opacity'),
+        imageUrl      = document.getElementById('imageUrl'),
+        saveImageBtn  = document.getElementById('saveImageUrl'),
+        rangeControls = [density, speed, rotation, opacity],
+        controls      = rangeControls.concat(imageUrl)
 
         defaults = {
             density: 50,
@@ -20,46 +21,50 @@
         initializeControls(controls, defaults, data['snowflakify-options']);
     });
 
+    // Image url events
+    saveImageBtn.onclick = saveImageUrl;
+    imageUrl.onkeydown = updateSaveButton;
+
     // Start snowflakify animation
     Snowflakify.initialize({imageUrl: chrome.extension.getURL('images/snowflake.png')});
     Snowflakify.startAnimation();
 
-    // Subscribe to the Save event
-    save.onclick = function(e) {
-        e.preventDefault();
-        var options = {},
-            i = 0,
-            control = null;
+    function saveSliderSettings() {
+        chrome.storage.local.get('snowflakify-options', updateSliderSettings);
+    }
 
-        // Construct options object
-        for(i; i < controls.length; i++) {
-            control = controls[i];
-            if (control.type == 'range') options[control.id] = parseInt(control.value, 10);
-            else options[control.id] = control.value;
-        }
+    function saveImageUrl() {
+        chrome.storage.local.get('snowflakify-options', updateImageUrl);
+    }
 
-        chrome.storage.local.set({'snowflakify-options': options});
+    function updateSliderSettings(data){
+        data = data['snowflakify-options'];
 
-        save.className = "button";
-        save.value = "Saved!";
+        // Get the updated slider values
+        _.forEach(rangeControls, function(control) {
+            data[control.id] = control.value;
+        });
 
-        // Restart the animation so user can see updated settings in action!
-        options.imageUrl || (options.imageUrl = chrome.extension.getURL('images/snowflake.png'));
-        Snowflakify.restartAnimation(options);
-    };
+        // Update the snowflakify options with the new slider values
+        chrome.storage.local.set({'snowflakify-options': data}, function() { restartAnimation(data) });
+    }
 
-    function initializeControls(controls, defaults, options) {
-        var val = null,
-            i = 0,
-            control = null;
+    function updateImageUrl(data){
+        data = data['snowflakify-options'];
+        data[imageUrl.id] = imageUrl.value
+
+        // Update the image url
+        chrome.storage.local.set({'snowflakify-options': data}, function() { restartAnimation(data) });
+    }
+
+    function initializeControls(options, defaults) {
+        var val = null;
 
         options = options || {};
         defaults = defaults || {};
 
-        // Set the slider value of each control
-        for(i; i < controls.length; i++) {
-            control = controls[i];
-
+        // Initialize each range control
+        _.forEach(controls, function(control) {
             // Get saved/default value for this control
             val = options[control.id] || defaults[control.id];
 
@@ -68,14 +73,27 @@
             if (val < 0) val = 0;
             if (val > 100) val = 100;
 
+            // Finally assign the control's value
             control.value = val;
-            control.onmousedown = saveDirty;
+
+            // Assign onmousedown handler for range controls
+            if (control.type === 'range') control.onmouseup = saveSliderSettings;
+        });
+    }
+
+    function updateSaveButton(e) {
+        saveImageBtn.className = "dirty button";
+        saveImageBtn.value = "Save";
+    }
+
+    function restartAnimation(data) {
+        // Set default picture if not specified
+        if (!data.imageUrl) {
+            data.imageUrl = chrome.extension.getURL('images/snowflake.png');
         }
+
+        // Finally, restart animation
+        Snowflakify.restartAnimation(data);
     }
 
-    function saveDirty(e) {
-        save.className = "dirty button";
-        save.value = "Save";
-    }
-
-})(Snowflakify, chrome);
+})(Snowflakify, _, chrome);
