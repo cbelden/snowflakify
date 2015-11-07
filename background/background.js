9e905c3030b84f8fdff28b19c88667cbdf9a4b3e
Calvin Belden <calvinbelden@gmail.com>
2015-11-07 09:23:55 -0600

    Fix folder structure. Prepare for build

--- background/background.js
+++
@@ -1,9 +0,0 @@
-(function(chrome) {
-    chrome.browserAction.onClicked.addListener(toggleFlakes);
-
-    function toggleFlakes(tabs) {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {action: 'toggle-flakes'});
-        });
-    }
-})(chrome);
